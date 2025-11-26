11af02cf8b3dd947eb65d09a010b1d7726a646f8
davidleon2208-hash <davidleon2208@gmail.com>
2025-11-26 14:58:20 -0300

    atualização

---
+++ json.js
@@ -0,0 +1,8 @@
+// 1. Obter o elemento
+const meuBotao = document.getElementById('.module-item disabled');
+
+// 2. Desativar
+meuBotao.disabled = true;
+
+// 3. Reativar (se necessário)
+// meuBotao.disabled = false;
